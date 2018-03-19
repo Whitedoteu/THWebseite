8447c352a583b4472db3daea91ea1768b393f7bb
Tobias Heer <tobias.heer@XEPTUM.local>
2018-03-19 16:04:49 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('link-to-mongodb', (err, database) => {
+  // ... start the server
+})
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+// All your handlers here...
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+  })
