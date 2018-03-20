c5db5112d093fa7cd679feeda53e92fba8917ec6
Tobias Heer <tobias.heer@XEPTUM.local>
2018-03-20 11:29:47 +0100

    final app for testing

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-var uri = "mongodb://mongo/app";
+const uri = "mongodb://mongo/app";
 const MongoClient = require('mongodb').MongoClient;
+const PORT = 3000;
+const HOST = '0.0.0.0';
 
-MongoClient.connect('your-mongodb-url', (err, client) => {
+
+MongoClient.connect(uri, (err, client) => {
   if (err) return console.log(err)
+  console.log("DB is fine")
   db = client.db('maindb') // whatever your database name is
-  app.listen(3000, () => {
-    console.log('listening on 3000')
-  })
 });
 
 //test
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.set('view engine', 'ejs')
 // All your handlers here...
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, HOST);
 
 app.get('/', (req, res) => {
-  db.collection('quotes').find().toArray(function(err, results) {
-    console.log(results)
+  
+  db.collection('quotes').find().toArray(function(err, result) {
+    if (err) return console.log(err)
     // send HTML file populated with quotes here
+    res.render('index.ejs', {quotes: result})
   })
+  
 });
 
 app.post('/quotes', (req, res) => {
