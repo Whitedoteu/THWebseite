2a9a1a0bd8862451a7579095457e51d0adcb6a0a
Tobias Heer <tobias.heer@XEPTUM.local>
2018-03-20 10:04:30 +0100

    added Docker and cleanup

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
-const express = require('express')
-const bodyParser= require('body-parser')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+var uri = "mongodb://mongo/app";
+const MongoClient = require('mongodb').MongoClient;
 
-const MongoClient = require('mongodb').MongoClient
+MongoClient.connect('your-mongodb-url', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('maindb') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+});
 
-MongoClient.connect('link-to-mongodb', (err, database) => {
-  // ... start the server
-})
 //test
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 
 // All your handlers here...
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
-app.get('/',(req,res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray(function(err, results) {
+    console.log(results)
+    // send HTML file populated with quotes here
+  })
+});
 
 app.post('/quotes', (req, res) => {
-    console.log(req.body)
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
   })
+  });
